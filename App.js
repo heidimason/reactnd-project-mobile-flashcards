5455af8882cd750dcfd7d59fc616e6d7fb165751
Heidi Mason <heidi@ipcprint.com>
2018-06-28 18:16:48 -0700

    feat: Add bottom tab nav

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
-import { Text, View, StatusBar } from 'react-native'
+import { Text, View, StatusBar, Platform } from 'react-native'
 import styled from 'styled-components/native'
-import { black } from './utils/colors'
+import { black, white } from './utils/colors'
 import { Constants } from 'expo'
+import { createBottomTabNavigator } from 'react-navigation'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import RF from 'react-native-responsive-fontsize'
 import ListDecks from './components/ListDecks'
 import ViewDeck from './components/ViewDeck'
 
@@ -18,6 +21,51 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
     )
 }
 
+const Tabs = createBottomTabNavigator({
+    ListDecks: {
+        screen: ListDecks,
+        navigationOptions: {
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ tintColor }) =>
+                <Ionicons
+                    name='ios-home'
+                    size={RF(4)}
+                    color={tintColor}
+                />
+        },
+    },
+    ViewDeck: {
+        screen: ViewDeck,
+        navigationOptions: {
+            tabBarLabel: 'Add Entry',
+            tabBarIcon: ({ tintColor }) =>
+                <FontAwesome
+                    name='plus-square'
+                    size={RF(4)}
+                    color={tintColor}
+                />
+        },
+    },
+}, {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? black : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === 'ios' ? white : black,
+            shadowColor: 'rgba(0, 0, 0, 0.24)',
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+})
+
 class FlashcardsApp extends Component {
     render() {
         return (
@@ -27,7 +75,7 @@ class FlashcardsApp extends Component {
                     barStyle="light-content"
                 />
 
-                <ViewDeck />
+                <Tabs />
             </ContainerView>
         )
     }
