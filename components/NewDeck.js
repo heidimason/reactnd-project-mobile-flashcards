519daf1eeb3361bdbd031515ccbc0a7f39b84ca9
Heidi Mason <heidi@ipcprint.com>
2018-07-06 13:32:33 -0700

    feat: Add flow for new deck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, TextInput } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import { Entypo } from '@expo/vector-icons'
 import styled from 'styled-components/native'
 import { fonts } from '../utils/fonts'
@@ -7,8 +7,32 @@ import { forms } from '../utils/forms'
 import { white, black } from '../utils/colors'
 import { btns } from '../utils/btns'
 import SubmitBtn from './SubmitBtn'
+import { NavigationActions } from 'react-navigation'
 
 class NewDeck extends Component {
+    state = {
+        title: ''
+    }
+
+    changeTitle = e => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    toHome = () => {
+        this.props.navigation.dispatch(
+            NavigationActions.back({key: 'NewDeck'})
+        )
+    }
+
+    addDeck = e => {
+        e.preventDefault()
+
+        // Navigate to home
+        this.toHome()
+    }
+
     render() {
         return (
             <ContainerView>
@@ -26,11 +50,14 @@ class NewDeck extends Component {
                             placeholder="Deck Title"
                             style={[fonts.h3, forms.textInput]}
                             selectionColor={black}
-                            underlineColorAndroid='rgba(0,0,0,0)'
+                            underlineColorAndroid="rgba(0,0,0,0)"
+                            value={this.state.title}
+                            onChange={this.changeTitle}
                         />
 
                         <SubmitBtn
-                            style={{backgroundColor: black, marginTop: '10%'}}>Submit
+                            style={{backgroundColor: black, marginTop: '10%'}}
+                            onPress={this.addDeck}>Submit
                         </SubmitBtn>
                     </View>
                 </CenterView>
